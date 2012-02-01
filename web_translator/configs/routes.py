578ceb1dcf565448933ce564b6aca446fb372aca
Sebastian Valenzuela <sebastiandfg@gmail.com>
2012-02-01 17:22:19 -0300

    Filter and add urls

--- web_translator/configs/routes.py
+++ web_translator/configs/routes.py
@@ -4,6 +4,9 @@ from web_translator.controllers import po_edit
 ROUTES = [
     {'url': '/', 'method': 'GET', 'controller': po_edit.select_language},
     {'url': '/translate/:lang_to', 'method':'GET', 'controller': po_edit.system_translate},
-    {'url': '/translate/:lang_to', 'method': 'POST', 'controller': po_edit.save_po},
-    {'url': '/generate_db', 'method': 'GET', 'controller': po_edit.generate_db}
+    {'url': '/translate/:lang_to/:role', 'method': 'GET', 'controller': po_edit.system_translate_by_role},
+    {'url': '/translate/:lang_to', 'method': 'POST', 'controller': po_edit.save_translation},
+    {'url': '/generate_db', 'method': 'GET', 'controller': po_edit.generate_db},
+    {'url': '/generate_po/:lang_to', 'method': 'GET', 'controller': po_edit.generate_po_from_db},
+    {'url': '/generate_all_po', 'method': 'GET', 'controller': po_edit.generate_all_po}
 ]
--- web_translator/controllers/po_edit.py
+++ web_translator/controllers/po_edit.py
@@ -14,7 +14,7 @@ def generate_db():
         sql_query = "CREATE table messages(id text not null primary key"
         for lang in LANGUAGES:
             sql_query += "," + lang[0] + " text"
-        sql_query += ",user_level text)"
+        sql_query += ",user_level text, url text)"
         request.db_cursor.execute(sql_query)
 
     for lang in LANGUAGES:
@@ -26,32 +26,65 @@ def generate_db():
             else:
                 request.db_cursor.execute("insert into messages(" + lang[0] + ",id) values(?, ?)", (msg.msgstr, msg.msgid))
 
+    return 'Database generated'
+
 
 def scrub(name):
     return ''.join( chr for chr in name if chr.isalnum() )
 
 #@secure('admin','system-translator')
 def system_translate(lang_to):
-    if lang_to:
-        messages = request.db_cursor.execute("select id, " + scrub(lang_to) + ", user_level from messages")
-        print messages.fetchone()
+    if lang_to in [lang[0] for lang in LANGUAGES]:
+        messages = request.db_cursor.execute("select id, " + lang_to + ", user_level, url from messages")
         return render_template('system_translation.html', msgs=messages, lang_to=lang_to, levels=TRANSLATOR_LEVELS)
     else:
         raise BadRequestException()
+
+def system_translate_by_role(lang_to, role):
+    if lang_to in [lang[0] for lang in LANGUAGES]:
+        messages = request.db_cursor.execute("select id, " + lang_to + ", user_level, url from messages where user_level=?", (role,))
+        return render_template('system_translation.html', msgs=messages, lang_to=lang_to, role=role)
+    else:
+        raise BadRequestException()
+
 
 #@secure('admin','system-translator')
 def select_language():
     return render_template('select_language.html')
 
 #@secure('admin','system-translator')
-def save_po(lang_to):
-    lang_to_messages = pofile(LOCALE_PATH + '/' + lang_to + '/LC_MESSAGES/epistemonikos.po', check_for_duplicates=True)
-    print request.POST.keys()
-    for msgid in request.POST.keys():
-        if msgid not in ['submit','lang_to']:
-            lang_to_messages.append(POEntry(msgid=msgid,msgstr=request.POST.get(msgid)))
+def save_translation(lang_to):
+    if lang_to in [lang[0] for lang in LANGUAGES]:
+        msgids = request.POST.dict.get('msgid[]')
+        msgstrs = request.POST.dict.get('msg[]')
+        msglvl = request.POST.dict.get('msglvl[]')
+        msgurl = request.POST.dict.get('msgurl[]')
+        for i in range(len(msgids)):
+            request.db_cursor.execute("UPDATE messages set "
+                                      + lang_to.decode('utf-8')
+                                      + "=?, user_level=?, url=? where id=?",
+                                        (msgstrs[i].decode('utf-8'),
+                                         msglvl[i].decode('utf-8'),
+                                         msgurl[i].decode('utf-8'),
+                                         msgids[i].decode('utf-8')))
 
-    lang_to_messages.save()
-    
-    return select_language()
-    
+        return 'Translated messages saved for %s' % [lang[1] for lang in LANGUAGES if lang[0] == lang_to][0]
+
+def generate_po_from_db(lang_to):
+    if lang_to in [lang[0] for lang in LANGUAGES]:
+        po_path = LOCALE_PATH + '/' + lang_to + '/LC_MESSAGES/epistemonikos.po'
+        mo_path = LOCALE_PATH + '/' + lang_to + '/LC_MESSAGES/epistemonikos.mo'
+        messages = request.db_cursor.execute("select id, " + lang_to + " from messages")
+        new_file = POFile()
+        for msg in messages.fetchall():
+            new_file.append(POEntry(msgid=msg[0],msgstr=msg[1]))
+        new_file.save(po_path)
+        new_file.save_as_mofile(mo_path)
+
+        return '.po file generated and compiled for %s' % [lang[1] for lang in LANGUAGES if lang[0] == lang_to][0]
+
+def generate_all_po():
+    msg = ''
+    for lang in LANGUAGES:
+        msg += generate_po_from_db(lang[0]) +'/n'
+    return msg
