03dc398a762a763607ce060f5711037a625902ee
Francisco Madrid <fjmadrid@gmail.com>
2015-02-10 19:54:16 -0300

    adding environment variable as database path

--- web_translator/configs/__init__.py
+++ web_translator/configs/__init__.py
@@ -3,17 +3,18 @@ import os
 
 TEMPLATES_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../templates")
 LOCALE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../locale")
-DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../db")
+DB_PATH = os.environ.get('SQLITE3_DB_PATH') or os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../db")
 TRANSLATOR_LEVELS = ['Adminstrator', 'Basic', 'Expert', 'PDQ', 'Delete']
 LOCALE_NAME = 'epistemonikos'
 ASSETS_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../assets")
 LANGUAGES = [
     ('ar', 'العربية'),
-	('de', 'Deutsch'),
-	('en', 'English') ,
-	('es', 'Español' ),
-	('fr', 'Français'),
-	('it', 'Italiano'),
-	('nl', 'Nederlands'),  ('pt', 'Português'),
-	('zh', '中文')
+    ('de', 'Deutsch'),
+    ('en', 'English'),
+    ('es', 'Español'),
+    ('fr', 'Français'),
+    ('it', 'Italiano'),
+    ('nl', 'Nederlands'),
+    ('pt', 'Português'),
+    ('zh', '中文')
 ]
